163d8d79a5d1f58e3db8caa86f8c596931b33b91
Carlo Costino <carlo.costino@wisertogether.com>
2012-08-13 15:28:24 -0400

    Fixed the folder name, Github URL, and a grammar change in the README.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='An abstract model providing common fields for Django models.',
     author='WiserTogether Tech Team',
     author_email='tech@wisertogether.com',
-    url='http://github.com/wisertoghether/django-base-model/',
+    url='http://github.com/wisertogether/django-base-model/',
     long_description=open('README', 'r').read(),
     packages=[
         'django_base_model',

9e4a6c0296b15d4ca1327107adac42fc3978c80d
Carlo Costino <carlo.costino@wisertogether.com>
2012-08-14 16:10:35 -0400

    Enhanced the display of the last modified by user.

--- django_base_model/admin.py
+++ django_base_model/admin.py
@@ -11,7 +11,15 @@ class BaseModelAdmin(admin.ModelAdmin):
     inherits from BaseModel.
     """
 
-    readonly_fields = ('last_modified_by', 'time_created', 'time_modified')
+    readonly_fields = (
+        'last_modified_by_name',
+        'time_created',
+        'time_modified'
+    )
+
+    def last_modified_by_name(self, obj):
+        return obj.last_modified_by.get_full_name()
+    last_modified_by_name.short_description = 'Last Modified By'
 
     def save_model(self, request, obj, form, change):
         if hasattr(obj, 'last_modified_by') and hasattr(request, 'user'):
